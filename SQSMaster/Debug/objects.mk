4c221d2d7a0a91481636d6885875a39665097582
yunmeng ban <banyunmeng@gmail.com>
2012-05-24 12:43:35 +0800

    Revert "add node code"
    This reverts commit d2b8c79be9086c9db17bb595394d0771c15f392e.
    
    	new file:   SQSMaster/Debug/SQSMaster
    	modified:   SQSMaster/Debug/makefile
    	modified:   SQSMaster/Debug/objects.mk
    	modified:   SQSMaster/Debug/src/subdir.mk

--- SQSMaster/Debug/objects.mk
+++ SQSMaster/Debug/objects.mk
@@ -5,3 +5,4 @@
 USER_OBJS :=
 
 LIBS := -lpthread -levent -levent_core -levent_extra
+
--- SQSMaster/Debug/src/subdir.mk
+++ SQSMaster/Debug/src/subdir.mk
@@ -4,26 +4,20 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/Database.cpp \
 ../src/Logger.cpp \
 ../src/MsgLock.cpp \
-../src/Node.cpp \
 ../src/SQSMaster.cpp \
 ../src/main.cpp 
 
 OBJS += \
-./src/Database.o \
 ./src/Logger.o \
 ./src/MsgLock.o \
-./src/Node.o \
 ./src/SQSMaster.o \
 ./src/main.o 
 
 CPP_DEPS += \
-./src/Database.d \
 ./src/Logger.d \
 ./src/MsgLock.d \
-./src/Node.d \
 ./src/SQSMaster.d \
 ./src/main.d 
 
@@ -32,7 +26,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I../../libevent-2.0.19/include/ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	g++ -I../../libevent-2.0.19/include/ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
